865168cd0499470541d3eebefc52aa5985fda373
Your Name <you@example.com>
2018-11-12 20:52:18 +0300

    fix

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-define('VIRTINA_VERSION', 143);
+define('VIRTINA_VERSION', 144);
 /*
 |--------------------------------------------------------------------------
 | Display Debug backtrace
